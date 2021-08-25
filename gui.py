3bb3f0cfe2a93aef5bd41b4f5b67cb195c454383
redbayoub <redayoub47@gmail.com>
2021-08-25 11:47:10 +0100

    refactor ui configs

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+from ui_conf_controller import UiConfController
 from constants import Constants
 import validators
 from backup_audio_recorder import BackupAudioRecorder
@@ -7,6 +8,7 @@ import os
 import json
 import tkinter as tk
 import pygubu
+
 # this is added to fix issues when using Pyinstaller command
 from pygubu.builder import tkstdwidgets
 from pygubu.builder import ttkstdwidgets
@@ -18,12 +20,6 @@ PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 PROJECT_UI = os.path.join(PROJECT_PATH, "gui.ui")
 
 
-USER_HOME_DIR_PATH = os.path.expanduser("~")
-PROJECT_LOCAL_DIR_PATH = os.path.join(USER_HOME_DIR_PATH, ".backup_audio_recorder")
-PROJECT_CONF_PATH = os.path.join(PROJECT_LOCAL_DIR_PATH, "conf.json")
-PROJECT_DEFAULT_REC_OUTPUT_PATH = os.path.join(PROJECT_LOCAL_DIR_PATH, "recordings")
-
-
 class GuiApp:
     def __init__(self, master=None):
         self.builder = builder = pygubu.Builder()
@@ -33,100 +29,108 @@ class GuiApp:
 
         builder.connect_callbacks(self)
 
-        os.makedirs(PROJECT_DEFAULT_REC_OUTPUT_PATH, exist_ok=True)
+        self.conf = UiConfController()
 
-        self.conf_data = {}
-        if not os.path.isfile(PROJECT_CONF_PATH):
-            self.conf_data = {
-                "output_directory": PROJECT_DEFAULT_REC_OUTPUT_PATH,
-                "dur_days": 0,
-                "dur_hours": 0,
-                "dur_minutes": 0,
-                "dur_seconds": 10,
-                "startup_recording": False,
-                "input_device": None,
-                "output_device": None,
-            }
-            json.dump(self.conf_data, open(PROJECT_CONF_PATH, "w"))
-        else:
-            self.conf_data = json.load(open(PROJECT_CONF_PATH))
-        
         # if output directory doesn't exists set to default
-        if not os.path.isdir(self.conf_data["output_directory"]):
-            self.conf_data["output_directory"] = PROJECT_DEFAULT_REC_OUTPUT_PATH
-            json.dump(self.conf_data, open(PROJECT_CONF_PATH, "w"))
+        if not os.path.isdir(self.conf.get_conf("output_directory")):
+            self.conf.set_conf(
+                "output_directory", self.conf.get_default_conf("output_directory")
+            )
 
         # setup duration entries callbacks
-        self.builder.get_variable("dur_days").trace_add("write", self.on_duration_change)
-        self.builder.get_variable("dur_hours").trace_add("write", self.on_duration_change)
-        self.builder.get_variable("dur_minutes").trace_add("write", self.on_duration_change)
-        self.builder.get_variable("dur_seconds").trace_add("write", self.on_duration_change)
+        self.builder.get_variable("dur_days").trace_add(
+            "write", self.on_duration_change
+        )
+        self.builder.get_variable("dur_hours").trace_add(
+            "write", self.on_duration_change
+        )
+        self.builder.get_variable("dur_minutes").trace_add(
+            "write", self.on_duration_change
+        )
+        self.builder.get_variable("dur_seconds").trace_add(
+            "write", self.on_duration_change
+        )
 
-        #init entries
-        self.__set_entry("location_entry", self.conf_data["output_directory"])
-        self.__set_entry("days_dur_entry", self.conf_data["dur_days"])
-        self.__set_entry("hours_dur_entry", self.conf_data["dur_hours"])
-        self.__set_entry("minutes_dur_entry", self.conf_data["dur_minutes"])
-        self.__set_entry("seconds_dur_entry", self.conf_data["dur_seconds"])
-        self.builder.get_variable("startup_recording_state").set(self.conf_data["startup_recording"])
+        # init entries
+        self.__set_entry("location_entry", self.conf.get_conf("output_directory"))
+        self.__set_entry("days_dur_entry", self.conf.get_conf("dur_days"))
+        self.__set_entry("hours_dur_entry", self.conf.get_conf("dur_hours"))
+        self.__set_entry("minutes_dur_entry", self.conf.get_conf("dur_minutes"))
+        self.__set_entry("seconds_dur_entry", self.conf.get_conf("dur_seconds"))
+        self.builder.get_variable("startup_recording_state").set(
+            self.conf.get_conf("startup_recording")
+        )
 
         self.init_devices_menu()
 
         self.recorder = BackupAudioRecorder(
-            output_directory=self.conf_data["output_directory"],
+            output_directory=self.conf.get_conf("output_directory"),
             listening_finished_callback=self.listen,
             exporting_finished_callback=self.export,
             input_device=self.current_input_device,
             output_device=self.current_output_device,
         )
 
-
         if self.builder.get_variable("startup_recording_state").get():
             self.record()
 
     def init_devices_menu(self):
-        default_input_device=sounddevice.query_devices(kind='input')['name']
-        default_output_device=sounddevice.query_devices(kind='output')['name']
-        input_devices= [device['name'] for device in sounddevice.query_devices() if device['max_input_channels'] > 0]
-        output_devices= [device['name'] for device in sounddevice.query_devices() if device['max_output_channels'] > 0]
-        
-        self.current_input_device = self.conf_data["input_device"]
-        self.current_output_device = self.conf_data["output_device"]
+        default_input_device = sounddevice.query_devices(kind="input")["name"]
+        default_output_device = sounddevice.query_devices(kind="output")["name"]
+        input_devices = [
+            device["name"]
+            for device in sounddevice.query_devices()
+            if device["max_input_channels"] > 0
+        ]
+        output_devices = [
+            device["name"]
+            for device in sounddevice.query_devices()
+            if device["max_output_channels"] > 0
+        ]
 
-        self.builder.get_object('input_device_menu').config(values=[f'Default: {default_input_device}']+input_devices)
-        self.builder.get_object('input_device_menu').current(0 if not  self.current_input_device else input_devices.index(self.current_input_device)+1 )
-        self.builder.get_object('input_device_menu').bind('<<ComboboxSelected>>', self.on_input_device_selected)
-        
-        self.builder.get_object('output_device_menu').config(values=[f'Default: {default_output_device}']+output_devices)
-        self.builder.get_object('output_device_menu').current(0 if not  self.current_output_device else output_devices.index(self.current_output_device)+1)
-        self.builder.get_object('output_device_menu').bind('<<ComboboxSelected>>', self.on_output_device_selected)
+        self.current_input_device = self.conf.get_conf("input_device")
+        self.current_output_device = self.conf.get_conf("output_device")
 
+        self.builder.get_object("input_device_menu").config(
+            values=[f"Default: {default_input_device}"] + input_devices
+        )
+        self.builder.get_object("input_device_menu").current(
+            0
+            if not self.current_input_device
+            else input_devices.index(self.current_input_device) + 1
+        )
+        self.builder.get_object("input_device_menu").bind(
+            "<<ComboboxSelected>>", self.on_input_device_selected
+        )
 
-    def load_conf_data_from_output_dir(self, output_dir):
-        rec_conf_path = os.path.join(output_dir, Constants.CONF_FILENAME)
-        if os.path.isfile(rec_conf_path):
-            rec_conf_data = json.load(open(rec_conf_path))
-            self.__set_entry("days_dur_entry", 0)
-            self.__set_entry("hours_dur_entry", 0)
-            self.__set_entry("minutes_dur_entry", 0)
-            # we substract one because it is added in recordding thread directly
-            self.__set_entry("seconds_dur_entry", rec_conf_data["duration"]-1)
+        self.builder.get_object("output_device_menu").config(
+            values=[f"Default: {default_output_device}"] + output_devices
+        )
+        self.builder.get_object("output_device_menu").current(
+            0
+            if not self.current_output_device
+            else output_devices.index(self.current_output_device) + 1
+        )
+        self.builder.get_object("output_device_menu").bind(
+            "<<ComboboxSelected>>", self.on_output_device_selected
+        )
 
-    def on_duration_change(self,_,__,___):
+    def on_duration_change(self, _, __, ___):
         duration = self.__get_duration()
-        filesize=BackupAudioRecorder.get_estimated_filesize(duration)
+        filesize = BackupAudioRecorder.get_estimated_filesize(duration)
         self.builder.get_variable("estimated_filesize").set(filesize)
-
 
     def browse(self):
         output_dir_path = dialogs.ask_folder(
-            initialdir=self.conf_data["output_directory"]
+            initialdir=self.conf.get_conf("output_directory")
         )
         if output_dir_path:
             self.__set_entry("location_entry", output_dir_path)
             self.recorder.set_output_directory(output_dir_path)
-            self.load_conf_data_from_output_dir(output_dir_path)
-            self.__save_conf()
+            self.conf.set_conf(
+                "output_directory",
+                output_dir_path,
+            )
 
     def is_int(self, value):
         return validators.is_int(value)
@@ -149,7 +153,7 @@ class GuiApp:
             duration = self.__get_duration()
             if duration == 0:
                 return
-            self.__save_conf()
+            self.__save_duration()
             self.recorder.start_recording(duration)
             record_btn.config(text="Recording ...", background="#ff0000")
 
@@ -165,53 +169,71 @@ class GuiApp:
 
             self.recorder.start_exporting(export_file_path)
             export_btn.config(text="Exporting ...")
-    
+
     def startup_recording_callback(self):
-        self.__save_conf()
-    
-    def on_input_device_selected(self,event):
-        selected_input_device=self.builder.get_object('input_device_menu').get()
+        self.conf.set_conf(
+            "startup_recording",
+            self.builder.get_variable("startup_recording_state").get(),
+        )
+
+    def on_input_device_selected(self, event):
+        selected_input_device = self.builder.get_object("input_device_menu").get()
         if selected_input_device == self.current_input_device:
             return
-       
-        if selected_input_device.lower().startswith('default'):
-            self.current_input_device=None
-        else:
-            self.current_input_device=selected_input_device
-        self.recorder.set_input_device( self.current_input_device)
-        self.__save_conf()
 
-    def on_output_device_selected(self,event):
-        selected_output_device=self.builder.get_object('output_device_menu').get()
+        if selected_input_device.lower().startswith("default"):
+            self.current_input_device = None
+        else:
+            self.current_input_device = selected_input_device
+        self.recorder.set_input_device(self.current_input_device)
+        self.conf.set_conf(
+            "input_device",
+            self.current_input_device,
+        )
+
+    def on_output_device_selected(self, event):
+        selected_output_device = self.builder.get_object("output_device_menu").get()
         if selected_output_device == self.current_output_device:
             return
-       
-        if selected_output_device.lower().startswith('default'):
-            self.current_output_device=None
+
+        if selected_output_device.lower().startswith("default"):
+            self.current_output_device = None
         else:
-            self.current_output_device=selected_output_device
-        self.recorder.set_output_device( self.current_output_device)
-        self.__save_conf()
-        pass
+            self.current_output_device = selected_output_device
+        self.recorder.set_output_device(self.current_output_device)
+        self.conf.set_conf(
+            "output_device",
+            self.current_input_device,
+        )
 
     def run(self):
         self.mainwindow.mainloop()
 
-    def __save_conf(self):
-        self.conf_data["output_directory"] =self.builder.get_variable("location").get()
+    def __save_duration(self):
+        duration = self.get_duration_fields()
 
-        duration=self.get_duration_fields()
+        self.conf.set_conf(
+            "dur_seconds",
+            duration["dur_seconds"],
+            auto_save=False,
+        )
+        self.conf.set_conf(
+            "dur_minutes",
+            duration["dur_minutes"],
+            auto_save=False,
+        )
+        self.conf.set_conf(
+            "dur_hours",
+            duration["dur_hours"],
+            auto_save=False,
+        )
+        self.conf.set_conf(
+            "dur_days",
+            duration["dur_days"],
+            auto_save=False,
+        )
 
-        self.conf_data["dur_seconds"] = duration['dur_seconds']
-        self.conf_data["dur_minutes"] = duration['dur_minutes']
-        self.conf_data["dur_hours"] = duration['dur_hours']
-        self.conf_data["dur_days"] = duration['dur_days']
-
-        self.conf_data["startup_recording"] = self.builder.get_variable("startup_recording_state").get()
-        self.conf_data["input_device"] = self.current_input_device
-        self.conf_data["output_device"] = self.current_output_device
-
-        json.dump(self.conf_data, open(PROJECT_CONF_PATH, "w"))
+        self.conf.save_conf()
 
     def __set_entry(self, entry_id, value):
         entry = self.builder.get_object(entry_id)
@@ -229,24 +251,28 @@ class GuiApp:
         dur_hours = self.builder.get_variable("dur_hours").get()
         dur_days = self.builder.get_variable("dur_days").get()
 
-        dur_seconds = 0 if dur_seconds=="" else int(dur_seconds)
-        dur_minutes = 0 if dur_minutes=="" else int(dur_minutes)
-        dur_hours = 0 if dur_hours=="" else int(dur_hours)
-        dur_days = 0 if dur_days=="" else int(dur_days)
-        return {'dur_seconds':dur_seconds,'dur_minutes':dur_minutes,'dur_hours':dur_hours,'dur_days':dur_days, }
-
+        dur_seconds = 0 if dur_seconds == "" else int(dur_seconds)
+        dur_minutes = 0 if dur_minutes == "" else int(dur_minutes)
+        dur_hours = 0 if dur_hours == "" else int(dur_hours)
+        dur_days = 0 if dur_days == "" else int(dur_days)
+        return {
+            "dur_seconds": dur_seconds,
+            "dur_minutes": dur_minutes,
+            "dur_hours": dur_hours,
+            "dur_days": dur_days,
+        }
 
     def __get_duration(self):
         SECONDS_IN_MINUTE = 60
         SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60
         SECONDS_IN_DAY = SECONDS_IN_HOUR * 24
 
-        duration=self.get_duration_fields()
+        duration = self.get_duration_fields()
         return (
-            duration['dur_seconds']
-            + duration['dur_minutes'] * SECONDS_IN_MINUTE
-            + duration['dur_hours'] * SECONDS_IN_HOUR
-            + duration['dur_days'] * SECONDS_IN_DAY
+            duration["dur_seconds"]
+            + duration["dur_minutes"] * SECONDS_IN_MINUTE
+            + duration["dur_hours"] * SECONDS_IN_HOUR
+            + duration["dur_days"] * SECONDS_IN_DAY
         )
 
 
---
+++ ui_conf_controller.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+USER_HOME_DIR_PATH = os.path.expanduser("~")
+PROJECT_LOCAL_DIR_PATH = os.path.join(USER_HOME_DIR_PATH, ".backup_audio_recorder")
+PROJECT_CONF_PATH = os.path.join(PROJECT_LOCAL_DIR_PATH, "conf.json")
+PROJECT_DEFAULT_REC_OUTPUT_PATH = os.path.join(PROJECT_LOCAL_DIR_PATH, "recordings")
+
+
+class UiConfController:
+    DEFAULT_CONF = {
+        "output_directory": PROJECT_DEFAULT_REC_OUTPUT_PATH,
+        "dur_days": 0,
+        "dur_hours": 0,
+        "dur_minutes": 0,
+        "dur_seconds": 10,
+        "startup_recording": False,
+        "input_device": None,
+        "output_device": None,
+    }
+
+    def __init__(self):
+
+        os.makedirs(PROJECT_DEFAULT_REC_OUTPUT_PATH, exist_ok=True)
+
+        if not os.path.isfile(PROJECT_CONF_PATH):
+            self.load_default_conf()
+        else:
+            try:
+                self.load_conf()
+            except json.decoder.JSONDecodeError:
+                self.load_default_conf()
+
+    def load_default_conf(self):
+        print('loading default conf')
+        self.conf_data = self.DEFAULT_CONF
+        self.save_conf()
+
+    def save_conf(self):
+        json.dump(self.conf_data, open(PROJECT_CONF_PATH, "w"))
+
+    def load_conf(self):
+        self.conf_data = json.load(open(PROJECT_CONF_PATH))
+
+    def get_conf(self, key):
+        try:
+            return self.conf_data[key]
+        except KeyError:
+            return self.DEFAULT_CONF[key]   
+            
+    def get_default_conf(self, key):
+        return self.DEFAULT_CONF[key]
+
+    def set_conf(self, key, value, auto_save=True):
+        self.conf_data[key] = value
+        if auto_save:
+            self.save_conf()
